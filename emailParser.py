fdc306bf0bb7bee9e02a413296263c0651bb4923
Travis <ts.large@gmail.com>
2019-06-27 11:47:26 -0400

    convert variables to camel case

--- emailParser.py
+++ emailParser.py
@@ -8,32 +8,32 @@ import email
 # from email.parser import Parser, BytesParser, HeaderParser
 # from email.policy import default, compat32
 
-# eml_file_path = 'samples/<spam email>.eml'
-eml_file_path = sys.argv[1]
+# emlFilePath = 'samples/<spam email>.eml'
+emlFilePath = sys.argv[1]
 
-with open(eml_file_path, 'r') as fp:
+with open(emlFilePath, 'r') as fp:
     data = fp.read()
     msg = email.message_from_string(data)
-    header_dict = dict()
+    headerDict = dict()
     for i in msg._headers:
-        header_dict[ i[0].strip(':') ] = i[1]
-    pprint.pprint(header_dict)
+        headerDict[ i[0].strip(':') ] = i[1]
+    pprint.pprint(headerDict)
     # print(msg.__dict__)
 
-    if 'multipart' in header_dict['Content-Type']:  # Email prob has attachments
-        msg_str = msg._payload[0]                   # meaning _payload is list
+    if 'multipart' in headerDict['Content-Type']:  # Email prob has attachments
+        msgStr = msg._payload[0]                   # meaning _payload is list
     else:
-        msg_str = msg._payload
+        msgStr = msg._payload
 
-    if type(msg_str) != str:
-        msg_str = msg_str.as_string()
+    if type(msgStr) != str:
+        msgStr = msgStr.as_string()
 
-    link_search = re.findall(r'http[s]?://[^\'" ]+', msg_str)
-    a_search = re.findall(r'<a href="(.*)" .*>', msg_str)
-    print('=== link_search ===\n')
-    if link_search is not None:
-        for i in link_search:
+    linkSearch = re.findall(r'http[s]?://[^\'" ]+', msgStr)
+    aSearch = re.findall(r'<a href="(.*)" .*>', msgStr)
+    print('=== linkSearch ===\n')
+    if linkSearch is not None:
+        for i in linkSearch:
             print(i)
-    print('\n=== a_search ===\n')
-    for a in a_search:
+    print('\n=== aSearch ===\n')
+    for a in aSearch:
         print(a)
